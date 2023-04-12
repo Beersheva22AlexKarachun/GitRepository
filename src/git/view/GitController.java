f49c21eecfe663c7aaab6f9910e20cc0f3746525
Alex Karachun <alexkarachun42@gmail.com>
2023-04-12 22:02:04 +0300

    Menu is ready

--- src/git/view/GitController.java
+++ src/git/view/GitController.java
@@ -1,14 +1,8 @@
 package git.view;
 
-import java.util.List;
-
-import git.tools.FileState;
 import git.tools.GitRepository;
-import telran.view.InputOutput;
 
 public class GitController {
-	private static final String ERROR_MSG = "Wrong input";
-	private static final String DATE_FORMAT = "dd.MM.yyyy";
 	private static final String MENU_NAME = "Git menu";
 	private GitRepository git;
 
@@ -18,11 +12,12 @@ public class GitController {
 
 	public Menu getMenu() {
 		return new Menu(MENU_NAME, Item.of("Commit", this::commit), Item.of("Info", this::info),
-				Item.of("Create branch", this::createBranch), Item.of("Rename branch", this::renameBranch),
-				Item.of("Delete branch", this::deleteBranch), Item.of("Log", this::log),
+				Item.of("Create a branch", this::createBranch), Item.of("Rename a branch", this::renameBranch),
+				Item.of("Delete a branch", this::deleteBranch), Item.of("Log", this::log),
 				Item.of("Branches", this::branches), Item.of("Commit content", this::commitContent),
 				Item.of("switchTo", this::switchTo), Item.of("getHead", this::getHead),
-				Item.of("addIgnoredFileNameExp", this::addIgnoredFileNameExp), Item.of("Exit", this::exit, true));
+				Item.of("Add an ignored filename expression", this::addIgnoredFileNameExp),
+				Item.of("Exit", (io) -> git.save(), true));
 	}
 
 	private void commit(InputOutput io) {
@@ -33,38 +28,49 @@ public class GitController {
 	private void info(InputOutput io) {
 		git.info().forEach(io::writeLine);
 	}
-	
+
 	private void createBranch(InputOutput io) {
 		String branchName = io.readString("Enter a new branch name:");
 		io.writeLine(git.createBranch(branchName));
 	}
-	
+
 	private void renameBranch(InputOutput io) {
 		String branchName = io.readString("Enter a branch name:");
 		String newbranchName = io.readString("Enter a branch new name:");
 		io.writeLine(git.renameBranch(branchName, newbranchName));
 	}
-	
+
 	private void deleteBranch(InputOutput io) {
 		String branchName = io.readString("Enter a branch name:");
 		io.writeLine(git.deleteBranch(branchName));
 	}
-	
+
 	private void log(InputOutput io) {
 		git.log().forEach(io::writeLine);
 	}
-	
+
 	private void branches(InputOutput io) {
 		git.branches().forEach(io::writeLine);
 	}
-	
+
 	private void commitContent(InputOutput io) {
 		String commitName = io.readString("Enter a name of branch/commit:");
 		git.commitContent(commitName).forEach(io::writeLine);
 	}
-	
+
 	private void switchTo(InputOutput io) {
 		String commitName = io.readString("Enter a name of branch/commit:");
 		io.writeLine(git.switchTo(commitName));
 	}
+
+	private void getHead(InputOutput io) {
+		String head = git.getHead();
+		io.writeLine(head != null ? head : "Head's on commit");
+	}
+
+	private void addIgnoredFileNameExp(InputOutput io) {
+		String regex = io.readString("Enter an expression:");
+		io.writeLine(git.addIgnoredFileNameExp(regex));
+	}
+
 }
